b3eed7e35ef36d19a9bc9a47d0d325daa2095f05
VKhushi07 <94143199+VKhushi07@users.noreply.github.com>
2022-05-09 19:27:14 -0400

    Final upload

---
+++ quiz4.js
@@ -0,0 +1,489 @@
+var quiz = 
+{
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+   {
+    q : "121. PSISA stands for Private Security Investigative Services Act. This act regulates the conduct of Security Guards",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "122. WHMIS Class F Includes",
+    o : [
+      "Compressible Gas",
+      "Dangerously Reactive Material",
+      "Oxidizing Material",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "123. Any citizen can arrest on a Found Committing criminal offence",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+  {
+    q : "124. While on the job, one of your co-workers continuously tells sexist jokes. He only does this when other men are present. You know other co-workers have told him that the jokes are inappropriate on several occasions. What is the most appropriate course of action?",
+    o : [
+      "Quit your job immediately",
+      "Inform a supervisor that the co-worker is telling sexist jokes",
+      "Keep it to yourself to protect your co-worker",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "125. The main purpose of the Occupational Health and Safety Act is",
+    o : [
+      "Protect workers",
+      "Establish procedures for identifying and dealing with workplace hazards",
+      "Enforcement for non-compliance",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "126. You are on patrol in a mall as a Security guard and you see a male smoking a Joint (Marijuana) on the property outside the east doors. What would be the best course of action",
+    o : [
+      "Arrest the male and seize the drugs, call police",
+      "Take the joint from the male and turn over it over to police",
+      "You have no authority to arrest for or seize the drugs. You should ask the male to leave the premises for a prohibited activity, make note of his description then contact police",
+      "Contact the police and await a search warrant"
+    ],
+    a : 2
+  },
+   {
+    q : "127. In a Hybrid offence, the Crown prosecutor has the choice to proceed by summary conviction or by indictment",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "128. The Best Evidence Rule refers to the best evidence available such as an original document over a copy",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "129. Smoke detectors detect flame, heat AND compressed gasses",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "130. People on certain drugs can be unpredictable and feel little or no pain in a physical confrontation.",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "131. What could be a result of, after collecting and storing evidence, the Chain Of Custody being broken",
+    o : [
+      "Evidence may no longer be admissible in court",
+      "The accused could walk free",
+      "Professionalism would be in question",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "132. The physical acts by which a criminal offence is committed",
+    o : [
+      "Mens Rea",
+      "Actus Reus",
+      "Res Judicata",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "132. Which of the following is Provincial Legislation",
+    o : [
+      "PIPEDA Personal Information Protection and Electronic Documents Act",
+      "FIPPA Freedom Of Information And Protection Of Privacy",
+      "MFIPPA Municipal Freedom Of Information And Protection Of Privacy Act",
+      "IFIPPA International Freedom Of Information And Protection Of Privacy Act"
+    ],
+    a : 1
+  },
+  {
+    q : "132. What is the minimum age to be licensed in Ontario as a Security guard",
+    o : [
+      "16",
+      "18",
+      "19",
+      "21"
+    ],
+    a : 1
+  },
+   {
+    q : "133. FIPPA Freedom Of Information And Protection Of Privacy is",
+    o : [
+      "Federal",
+      "Provincial",
+      "Municipal",
+      "International"
+    ],
+    a : 1
+  },
+   {
+    q : "134. 10 code for acknowledge message",
+    o : [
+      "10-2",
+      "10-4",
+      "10-7",
+      "10-8"
+    ],
+    a : 1
+  },
+   {
+    q : "135. A complaint against a security guard can be reported by",
+    o : [
+      "Your supervisor",
+      "The client",
+      "People who go on your property",
+      "Anyone"
+    ],
+    a : 3
+  },
+   {
+    q : "136. A witness who testifies as to the habits and reputation of another person is giving what type of evidence",
+    o : [
+      "Circumstantial evidence",
+      "Character evidence",
+      "Trace evidence",
+      "Conscripted evidence"
+    ],
+    a : 1
+  },
+   {
+    q : "137. As a Security guard you are given a list of names and phone numbers by a client to call to see if you can get them to pay their outstanding bills owed to the client's company. As a Security guard is this a task you would be allowed to perform under the P.S.I.S.A.",
+    o : [
+      "Yes",
+      "No"
+    ],
+    a : 1
+  },
+   {
+    q : "139. Evidence of prior bad acts by the accused is called",
+    o : [
+      "Similar fact evidence",
+      "Circumstantial evidence",
+      "Bad luck evidence",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "140. 10 code for bomb threat",
+    o : [
+      "10-98",
+      "10-79",
+      "10-100",
+      "10-200"
+    ],
+    a : 2
+  },
+   {
+    q : "141. When a Security guard appears in court to testify he / she must",
+    o : [
+      "Always tell the truth",
+      "Dress professionally",
+      "Bring notes and any evidence relating to the case",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "142. Which of the following would not be considered a regular access control point",
+    o : [
+      "Door",
+      "Window",
+      "Turnstiles",
+      "Gate"
+    ],
+    a : 1
+  },
+   {
+    q : "143. 10 code for location",
+    o : [
+      "10-1",
+      "10-4",
+      "10-20",
+      "10-19"
+    ],
+    a : 2
+  },
+   {
+    q : "144. An Information is the name of a document used",
+    o : [
+      "To formally charge an offender",
+      "Is used to make notes about an incident",
+      "To compile intelligence",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "145. Section 494 (1) of the Criminal Code deals with:",
+    o : [
+      "Citizen arrest authority",
+      "Trespass on private property",
+      "Regulates conduct of a security guard",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "146. A Field Alarm would be mostly used to",
+    o : [
+      "Keep cows in a field",
+      "Protect valuable items such as in a museum",
+      "In conjunction with an electric fence",
+      "To keep trespassers out of a field"
+    ],
+    a : 1
+  },
+   {
+    q : "147. f an accused is acquitted of a criminal offence this means",
+    o : [
+      "He has been found not guilty of the offence",
+      "Sentenced to probation",
+      "A new trial date must be set",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "148. Conditional Discharge is when the conditions set by the court are satisfied, the discharge becomes absolute",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "149. 10 code for busy",
+    o : [
+      "10-6",
+      "10-1",
+      "10-9",
+      "10-5"
+    ],
+    a : 0
+  },
+   {
+    q : "150. Upon a person's arrest or detention section 10 (a) of the Charter Of Rights And Freedoms requires a person",
+    o : [
+      "to be informed promptly of the reasons",
+      "to retain and instruct counsel without delay and to be informed of that right",
+      "not to be compelled to be a witness",
+      "to be presumed innocent until proven guilty"
+    ],
+    a : 0
+  },
+   {
+    q : "151. Class A Fires Involve",
+    o : [
+      "Combustible Metals",
+      "Oxidizing Material",
+      "Ordinary Combustibles",
+      "Dangerously Reactive Material"
+    ],
+    a : 2
+  },
+   {
+    q : "152. Section 7 of the Charter Of Rights And Freedoms deals with",
+    o : [
+      "Search or Seizure",
+      "Life liberty and security of person",
+      "Detention or imprisonment",
+      "Arrest or detention"
+    ],
+    a : 1
+  },
+   {
+    q : "153. When using a fire extinguisher you should always aim the nozzle",
+    o : [
+      "At the flames directly",
+      "At the flames indirectly",
+      "At the top of the flames",
+      "At the base of the flames"
+    ],
+    a : 3
+  },
+   {
+    q : "154. What is the first action you should take when you note a breach of security while on routine patrol of a property?",
+    o : [
+      "Disregard it because it was probably someone from a previous shift",
+      "Immediately notify your supervisor of your location and the nature of the breach",
+      "Call the police and lock down the building",
+      "Search the building after calling for back up"
+    ],
+    a : 1
+  },
+   {
+    q : "155. 10 code for back in service",
+    o : [
+      "10-6",
+      "10-19",
+      "10-8",
+      "10-1"
+    ],
+    a : 2
+  },
+   {
+    q : "156. If you find a suspicious package you should call the police from your cell phone immediately to save time",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "157. Upon a person's arrest or detention section 10 (b) of the Charter Of Rights And Freedoms requires a person",
+    o : [
+      "To be informed of the right to retain and instruct council without delay",
+      "to be tried within a reasonable time",
+      "not to be denied reasonable bail",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "158. Employers who are aware that domestic violence may occur in the workplace must take every precaution reasonable in the circumstances to protect a worker at risk of physical injury",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "159. Under the Criminal Code of Canada, to justify the Use of Force what would be required",
+    o : [
+      "You must decide if the person may escape",
+      "You must be acting on reasonable grounds",
+      "A security guard can decide",
+      "The person keeps using profanity"
+    ],
+    a : 1
+  },
+   {
+    q : "160. In court, the person that usually hears bail applications, arraignments, traffic court is called a",
+    o : [
+      "Prosecutor",
+      "Court reporter",
+      "Justice Of The Peace",
+      "Judge"
+    ],
+    a : 2
+  }
+  ],
+
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+    
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) 
+      { 
+        quiz.draw(); 
+      }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
